cc385678bd07ef83d17a61205f20214f64578ea6
julesmuskala <30392252+julesmuskala@users.noreply.github.com>
2022-05-26 12:42:34 +0200

    Add API comments

--- json.h
+++ json.h
@@ -9,6 +9,10 @@
 
 namespace json {
 
+/**
+ * @class JSON
+ * Holds JSON file data, parses JSON strings and generates stringified data.
+*/
 class JSON {
   Value *value_;
   ValueType base_type_;
@@ -16,6 +20,9 @@ class JSON {
   bool delete_value_;
 
  public:
+  /**
+   * @param string - stringified JSON
+  */
   explicit JSON(const std::string &s) {
     Parser parser(s);
     value_ = parser.Parse();
@@ -27,13 +34,22 @@ class JSON {
 
   explicit JSON(Value &value) noexcept
       : value_(&value), base_type_(value.Type()), delete_value_(false) {}
-
+  /**
+   * @param int - indent amount; negative or no value will result if minified json
+  */
   [[nodiscard]] std::string Stringify() const noexcept {
     Generator generator(value_);
     return generator.generate();
   }
+  /**
+   * @param int - indent amount; negative or no value will result if minified json
+  */
   [[nodiscard]] std::string Stringify(int indent_amount) const noexcept {
     Generator generator(value_, indent_amount);
+    return generator.generate();
+  }
+  static std::string Stringify(JSON& json, int indent_amount) noexcept {
+    Generator generator(json.GetValue(), indent_amount);
     return generator.generate();
   }
 
